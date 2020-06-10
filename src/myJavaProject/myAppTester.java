7adedf3716e002767a9666c6b5f29a3a5ce4eda0
prabhjot <prabhhjot.kaur@gmail.com>
2020-06-10 13:16:09 -0700

    Program modified

--- src/myJavaProject/myAppTester.java
+++ src/myJavaProject/myAppTester.java
@@ -17,7 +17,8 @@ public class myAppTester {
 	  reverseMethod(n);
 	  System.out.println();
 
-	  // add a line below to call your myTestMethod from here  then remove the block comment symbols  
+	  System.out.print("Factorial of the input number is:");
+	  System.out.print(factorial(n));  
 
 	}
 	
@@ -34,14 +35,13 @@ public class myAppTester {
 	   }
 
 
-	   /* 
-	       add your test method in this space then remove the block comment symbols
-	       public static void myTestMethod(...){
-
-            
-
-		   }
-	 
-	   */
+	public static int factorial(int num)
+	{
+	 if(num>=1)
+		 return num*factorial(num-1);
+	 else
+		 return 1;
+	}
+	   
 
 }
